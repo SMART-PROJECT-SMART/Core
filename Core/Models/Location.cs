4b7e6f6246141a593a6c25cfc5da5cb31635e446
kalush666 <jonathan.kalush@gmail.com>
2025-12-14 08:43:37 +0200

    [feat] calculate distance to function

--- Core/Models/Location.cs
+++ Core/Models/Location.cs
@@ -5,5 +5,21 @@
         public double Latitude { get; set; } = latitude;
         public double Longitude { get; set; } = longitude;
         public double Altitude { get; set; } = altitude;
+
+        public double CalculateDistanceTo(Location targetLocation)
+        {
+            double latitudeDifference = Latitude - targetLocation.Latitude;
+            double longitudeDifference = Longitude - targetLocation.Longitude;
+            double altitudeDifference = Altitude - targetLocation.Altitude;
+
+            double horizontalDistanceSquared =
+                (latitudeDifference * latitudeDifference) + (longitudeDifference * longitudeDifference);
+            double verticalDistanceSquared = altitudeDifference * altitudeDifference;
+
+            double totalDistanceSquared = horizontalDistanceSquared + verticalDistanceSquared;
+            double distance = Math.Sqrt(totalDistanceSquared);
+
+            return distance;
+        }
     }
 }
