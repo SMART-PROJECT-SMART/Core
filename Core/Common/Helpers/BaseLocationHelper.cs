da35cc71b4872ac040870e0aee4c4f15916fd418
jonathan kalush <jonathan.kalush@gmail.com>
2026-03-19 09:11:16 +0200

    [minor[ location helper

--- Core/Common/Helpers/BaseLocationHelper.cs
+++ Core/Common/Helpers/BaseLocationHelper.cs
@@ -7,14 +7,14 @@ namespace Core.Common.Helpers
     {
         private static readonly Dictionary<BaseLocation, Location> _baseToLocation = new()
         {
-            { BaseLocation.Hatzerim, new Location(31.2333, 34.6625, 0) },
-            { BaseLocation.TelNof, new Location(31.8394, 34.8217, 0) },
-            { BaseLocation.RamatDavid, new Location(32.6656, 35.1817, 0) },
-            { BaseLocation.Nevatim, new Location(31.2083, 35.0122, 0) },
-            { BaseLocation.Ramon, new Location(30.7761, 34.6667, 0) },
-            { BaseLocation.Hatzor, new Location(31.7625, 34.7272, 0) },
-            { BaseLocation.Palmachim, new Location(31.8978, 34.6906, 0) },
-            { BaseLocation.Ovda, new Location(29.94, 34.9358, 0) }
+            { BaseLocation.Hatzerim, new Location(31.2333, 34.6625, 20) },
+            { BaseLocation.TelNof, new Location(31.8394, 34.8217, 20) },
+            { BaseLocation.RamatDavid, new Location(32.6656, 35.1817, 20) },
+            { BaseLocation.Nevatim, new Location(31.2083, 35.0122, 20) },
+            { BaseLocation.Ramon, new Location(30.7761, 34.6667, 20) },
+            { BaseLocation.Hatzor, new Location(31.7625, 34.7272, 20) },
+            { BaseLocation.Palmachim, new Location(31.8978, 34.6906, 20) },
+            { BaseLocation.Ovda, new Location(29.94, 34.9358, 20) }
         };
 
         public static Location GetLocation(BaseLocation baseLocation)
